f95d832a4b622f60f54d52e13d1c98fa700aed98
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-04-12 12:29:22 +0200

    Add missions initial structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import RocketsList from './components/RocketsList';
 import MyProfile from './components/MyProfile';
-import Missions from './components/Missions';
+import MissionsTable from './components/MIssionTable';
 
 function App() {
   return (
     <Router>
-          <div className="App">
-      <Navbar />
-      <Routes>
-      <Route exact path="/" element = {<RocketsList />} />
-      <Route path="/myprofile" element = {<MyProfile />} />
-      <Route path="/missions" element = {<Missions />} />
-              </Routes>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<RocketsList />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+          <Route path="/missions" element={<MissionsTable />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/components/MissionItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleJoinMission } from '../redux/missions/missionsSlice';
+// import './styles/MissionTable.css';
+
+const MissionItem = (props) => {
+  const dispatch = useDispatch();
+  const joinMission = useSelector(
+    (state) => state.mission.joinMission[props.id]
+  );
+
+  const handleJoinMission = (missionId) => {
+    dispatch(toggleJoinMission({ missionId }));
+  };
+
+  const membershipStatusClassName = joinMission ? 'activeMember' : 'notMember';
+
+  const joinMissionStatusClassnName = joinMission
+    ? 'leaveMission'
+    : 'joinMission';
+
+  return (
+    <tr className="table-row">
+      <td className="mission-name">{props.name}</td>
+      <td>{props.description}</td>
+      <td>
+        <span className={`membership-status ${membershipStatusClassName}`}>
+          {joinMission ? 'Active Member' : 'NOT A MEMBER'}
+        </span>
+      </td>
+      <td>
+        <button
+          type="button"
+          onClick={() => handleJoinMission(props.id)}
+          className={`joinMission-action ${joinMissionStatusClassnName}`}
+        >
+          {joinMission ? 'Leave Mission' : 'Join Mission'}
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default MissionItem;
