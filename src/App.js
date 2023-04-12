ecd48036b651f62381ee1ceea3626269615fe46a
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-04-12 11:37:41 +0530

    Add setup files
    Add setup files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import RocketsList from './components/RocketsList';
+import MyProfile from './components/MyProfile';
+import Missions from './components/Missions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+          <div className="App">
+      <Navbar />
+      <Routes>
+      <Route exact path="/" element = {<RocketsList />} />
+      <Route path="/myprofile" element = {<MyProfile />} />
+      <Route path="/missions" element = {<Missions />} />
+              </Routes>
     </div>
+    </Router>
   );
 }
 
